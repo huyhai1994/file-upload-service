53802ab635370668e5eb1ec30807ef940f2b401f
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-07-10 15:41:32 +0700

    add base case for file request verify service.

---
+++ src/main/java/org/mini_lab/file_upload_service/service/FileRequestVerifyService.java
@@ -0,0 +1,10 @@
+package org.mini_lab.file_upload_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FileRequestVerifyService {
+
+}
