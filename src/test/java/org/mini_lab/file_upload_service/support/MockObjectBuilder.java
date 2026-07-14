7de4e0908bb281dd19a961dedc9eb45a87530ec6
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-07-14 11:25:17 +0700

    refactor: move Mock FileMetadata to MockObjectBuilder

--- src/test/java/org/mini_lab/file_upload_service/support/MockObjectBuilder.java
+++ src/test/java/org/mini_lab/file_upload_service/support/MockObjectBuilder.java
@@ -2,6 +2,8 @@ package org.mini_lab.file_upload_service.support;
 
 import org.jetbrains.annotations.NotNull;
 import org.mini_lab.file_upload_service.dto.FileUploadCommand;
+import org.mini_lab.file_upload_service.entity.FileMetadata;
+import org.mini_lab.file_upload_service.entity.FileState;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -66,4 +68,48 @@ public class MockObjectBuilder {
                 pngHeader
         );
     }
+
+    public static @NotNull FileMetadata getValidUploadingFileMetadata() {
+        FileMetadata fileMetadata = new FileMetadata();
+
+        fileMetadata.setTitle("Avatar of John Doe");
+        fileMetadata.setFileName("avatar.png");
+        fileMetadata.setContentType("image/png");
+        fileMetadata.setExtension("png");
+        fileMetadata.setObjectKey("2026/07/08/8b7c3d0d-f0d1-4a7c-a3a4-7d4ef2e81a55.png");
+        fileMetadata.setSize(512_384L);
+        fileMetadata.setChecksum("9d5ed678fe57bcca610140957afab571f6d9f1f5e53e7d8d0b8f359bd2d96d8e");
+        fileMetadata.setStatus(FileState.UPLOADING);
+        return fileMetadata;
+    }
+
+    public static @NotNull FileMetadata getValidCompletedFileMetadata() {
+        FileMetadata fileMetadata = new FileMetadata();
+
+        fileMetadata.setTitle("Avatar of John Doe");
+        fileMetadata.setFileName("avatar.png");
+        fileMetadata.setContentType("image/png");
+        fileMetadata.setExtension("png");
+        fileMetadata.setObjectKey("2026/07/08/8b7c3d0d-f0d1-4a7c-a3a4-7d4ef2e81a55.png");
+        fileMetadata.setSize(512_384L);
+        fileMetadata.setChecksum("9d5ed678fe57bcca610140957afab571f6d9f1f5e53e7d8d0b8f359bd2d96d8e");
+        fileMetadata.setStatus(FileState.COMPLETED);
+        return fileMetadata;
+    }
+
+    public static FileMetadata getNonValidUploadingFileMetadata() {
+        FileMetadata fileMetadata = new FileMetadata();
+
+        fileMetadata.setTitle("Avatar of John Doe");
+        fileMetadata.setFileName(null);
+        fileMetadata.setContentType(null);
+        fileMetadata.setExtension("png");
+        fileMetadata.setObjectKey(null);
+        fileMetadata.setSize(512_384L);
+        fileMetadata.setChecksum("9d5ed678fe57bcca610140957afab571f6d9f1f5e53e7d8d0b8f359bd2d96d8e");
+        fileMetadata.setStatus(FileState.UPLOADING);
+        fileMetadata.setStatus(null);
+        return fileMetadata;
+    }
+
 }
