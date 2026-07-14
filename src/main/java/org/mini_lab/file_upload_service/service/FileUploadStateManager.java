b3049f486fdeb3e9ab3447aaf6910f3da4d980d8
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-07-14 10:26:51 +0700

    add FileUploadStateManager markCompleted and markFailed with test cases.

--- src/main/java/org/mini_lab/file_upload_service/service/FileUploadStateManager.java
+++ src/main/java/org/mini_lab/file_upload_service/service/FileUploadStateManager.java
@@ -1,6 +1,7 @@
 package org.mini_lab.file_upload_service.service;
 
 import lombok.RequiredArgsConstructor;
+import org.mini_lab.file_upload_service.exception.InvalidStateTransitionException;
 import org.mini_lab.file_upload_service.repository.FileMetadataRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,8 +12,20 @@ public class FileUploadStateManager {
     private final FileMetadataRepository fileMetadataRepository;
 
     @Transactional
-    public void markCompleted(Long fileId, String checkSum) {
+    public void markCompleted(Long fileId, String checksum) {
+        int affectedRowCount = fileMetadataRepository.markCompletedIfUploading(fileId, checksum);
+        assertOnlyOneAffectedRow(affectedRowCount);
+    }
 
+    @Transactional
+    public void markFailed(Long fileId) {
+        int affectedRowCount = fileMetadataRepository.markFailedIfUploading(fileId);
+        assertOnlyOneAffectedRow(affectedRowCount);
+    }
 
+    private void assertOnlyOneAffectedRow(int affectedRowCount) {
+        if (affectedRowCount != 1) {
+            throw new InvalidStateTransitionException();
+        }
     }
 }
