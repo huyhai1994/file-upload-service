3cad99d33f7f82368a15f837e2e58801bca6caed
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-07-16 11:06:14 +0700

    add missing size() field when extract metadata from multipart request.

--- src/test/java/org/mini_lab/file_upload_service/service/FileUploadRequestExtractorTest.java
+++ src/test/java/org/mini_lab/file_upload_service/service/FileUploadRequestExtractorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.mini_lab.file_upload_service.dto.FileUploadCommand;
 import org.mini_lab.file_upload_service.dto.UploadRequestObjectDTO;
 import org.mini_lab.file_upload_service.support.MockObjectBuilder;
+import org.springframework.web.multipart.MultipartFile;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,9 +13,11 @@ class FileUploadRequestExtractorTest {
 
     @Test
     void shouldBeExtractToFileUploadCommand() {
-        UploadRequestObjectDTO uploadRequestObjectDTO = new UploadRequestObjectDTO(MockObjectBuilder.getTextContentTypeMultipartFile());
+        MultipartFile file = MockObjectBuilder.getTextContentTypeMultipartFile();
+        UploadRequestObjectDTO uploadRequestObjectDTO = new UploadRequestObjectDTO(file);
         FileUploadCommand uploadCommand = fileUploadRequestExtractor.extract(uploadRequestObjectDTO);
         assertEquals("test.txt", uploadCommand.originalFileName());
         assertEquals("text/plain", uploadCommand.contentType());
+        assertEquals(file.getSize(),uploadCommand.size());
     }
 }
