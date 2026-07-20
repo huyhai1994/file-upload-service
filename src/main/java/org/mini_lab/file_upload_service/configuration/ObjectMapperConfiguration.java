8891621c605e9752efd66cd824649399bf642588
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-07-20 09:39:25 +0700

    add java time module into object mapper to support serialize Date time java 8

--- src/main/java/org/mini_lab/file_upload_service/configuration/ObjectMapperConfiguration.java
+++ src/main/java/org/mini_lab/file_upload_service/configuration/ObjectMapperConfiguration.java
@@ -3,11 +3,16 @@ package org.mini_lab.file_upload_service.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Configuration
 public class ObjectMapperConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
     }
 }
