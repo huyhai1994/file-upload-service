6d81392fd689c1834174d6f468b8b5e3a0b96967
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-07-27 14:21:21 +0700

    Configure CSRF and permit access to the register endpoint

--- src/main/java/org/mini_lab/file_upload_service/configuration/security/SercurityConfiguration.java
+++ src/main/java/org/mini_lab/file_upload_service/configuration/security/SercurityConfiguration.java
@@ -2,8 +2,11 @@ package org.mini_lab.file_upload_service.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SercurityConfiguration {
@@ -11,5 +14,20 @@ public class SercurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        httpSecurity
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/api/v1/auth/register")
+                )
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return httpSecurity.build();
     }
 }
