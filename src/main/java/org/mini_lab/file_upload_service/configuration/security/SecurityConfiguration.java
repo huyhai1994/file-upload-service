00f70b41ae677253305dd186435396a951f6aad5
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-07-29 14:43:36 +0700

    renamed class

--- src/main/java/org/mini_lab/file_upload_service/configuration/security/SecurityConfiguration.java
+++ src/main/java/org/mini_lab/file_upload_service/configuration/security/SecurityConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SercurityConfiguration {
+public class SecurityConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
