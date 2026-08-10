d5c6110260853b8c137bbff1e3ae99d1b7bbc28a
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-08-10 13:39:35 +0700

    change folder and turn off register jwt filter in servlet container.

--- src/main/java/org/mini_lab/file_upload_service/shared/configuration/SecurityConfiguration.java
+++ src/main/java/org/mini_lab/file_upload_service/shared/configuration/SecurityConfiguration.java
@@ -1,18 +1,19 @@
-package org.mini_lab.file_upload_service.security.authentication.configuration;
+package org.mini_lab.file_upload_service.shared.configuration;
 
 import org.mini_lab.file_upload_service.security.authentication.service.CustomUserDetailsManager;
 import org.mini_lab.file_upload_service.security.jwt.components.CustomAuthenticationEntryPoint;
 import org.mini_lab.file_upload_service.security.jwt.filter.JwtAuthenticationFilter;
 import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,6 +25,19 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public FilterRegistrationBean<JwtAuthenticationFilter>
+    jwtAuthenticationFilterRegistration(
+            JwtAuthenticationFilter filter
+    ) {
+        FilterRegistrationBean<JwtAuthenticationFilter> registration =
+                new FilterRegistrationBean<>(filter);
+
+        registration.setEnabled(false);
+
+        return registration;
     }
 
     @Bean
@@ -55,6 +69,7 @@ public class SecurityConfiguration {
 
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 EndpointRequest.to("health"),
